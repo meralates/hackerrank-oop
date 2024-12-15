4540ec2ed9c7af1c7ec7f4cb165867d9a80231ab
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-12-16 01:24:27 +0300

    java interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+
+//https://www.hackerrank.com/challenges/java-interface/problem?isFullScreen=true
 public class Main {
+    interface GeometricShape {
+        double area();
+    }
+    static class Circle implements GeometricShape {
+        private double radius;
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+        @Override
+        public double area() {
+            return Math.PI * radius * radius;
+        }
+    }
+    static class Rectangle implements GeometricShape {
+        private double length;
+        private double width;
+        public Rectangle(double length, double width) {
+            this.length = length;
+            this.width = width;
+        }
+        @Override
+        public double area() {
+            return length * width;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GeometricShape circle = new Circle(2.0);
+        GeometricShape rectangle = new Rectangle(3.0, 4.0);
+
+        System.out.println(circle.area());
+        System.out.println(rectangle.area());
     }
 }
