71edb2a2d9341c64ae884bd7ad1b4ca7c3ce38d4
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-12-19 10:58:07 +0300

    java interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+//https://www.hackerrank.com/challenges/java-interface/problem?isFullScreen=true
+interface AdvancedArithmetic{//bir tam sayı alıp bu tam sayının tüm pozitif bölenlerinin toplamını döndüren
+    int divisor_sum(int n);//divisor_sum(int n) adlı bir metodu tanımlar.
+
+}
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+class Solution{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
     }
 }
