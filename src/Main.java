0a9037296cf3b3ae78223a891fd70b4dd5b46326
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-12-19 10:32:03 +0300

    java inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,24 @@
 
-//https://www.hackerrank.com/challenges/java-interface/problem?isFullScreen=true
-public class Main {
-    interface GeometricShape {
-        double area();
-    }
-    static class Circle implements GeometricShape {
-        private double radius;
-        public Circle(double radius) {
-            this.radius = radius;
-        }
-        @Override
-        public double area() {
-            return Math.PI * radius * radius;
-        }
-    }
-    static class Rectangle implements GeometricShape {
-        private double length;
-        private double width;
-        public Rectangle(double length, double width) {
-            this.length = length;
-            this.width = width;
-        }
-        @Override
-        public double area() {
-            return length * width;
-        }
-    }
-    public static void main(String[] args) {
-        GeometricShape circle = new Circle(2.0);
-        GeometricShape rectangle = new Rectangle(3.0, 4.0);
+//https://www.hackerrank.com/challenges/java-inheritance-1/problem?isFullScreen=true
 
-        System.out.println(circle.area());
-        System.out.println(rectangle.area());
+class Animal{
+    void walk(){
+        System.out.println("I'm walking");
+    }
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I'm flying");
+    }
+    void sing(){
+        System.out.println("I'm singing");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+    Bird bird = new Bird();
+    bird.walk();
+    bird.fly();
+    bird.sing();
     }
 }
