cbf2d4bbc0d3e47d8710b02c4bf07b46a0244e6d
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-12-19 11:38:05 +0300

    java abstract class

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,30 @@
 import java.util.*;
-//https://www.hackerrank.com/challenges/java-interface/problem?isFullScreen=true
-interface AdvancedArithmetic{//bir tam sayı alıp bu tam sayının tüm pozitif bölenlerinin toplamını döndüren
-    int divisor_sum(int n);//divisor_sum(int n) adlı bir metodu tanımlar.
+//https://www.hackerrank.com/challenges/java-abstract-class/problem?isFullScreen=true
+abstract class Book {
+    String title;
+    abstract void setTitle(String s);
+    String getTitle() {
+        return title;
+    }
+}
 
-}
-class MyCalculator implements AdvancedArithmetic {
-    public int divisor_sum(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                sum += i;
-            }
-        }
-        return sum;
+class MyBook extends Book {
+    // Implementing the abstract method
+    @Override
+    void setTitle(String s) {
+        this.title = s;
     }
 }
-class Solution{
-    public static void main(String []args){
-        MyCalculator my_calculator = new MyCalculator();
-        System.out.print("I implemented: ");
-        ImplementedInterfaceNames(my_calculator);
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        String title = sc.nextLine();
+
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+
+        System.out.println("The title is: " + new_novel.getTitle());
+
         sc.close();
-    }
-    /*
-     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
-     */
-    static void ImplementedInterfaceNames(Object o){
-        Class[] theInterfaces = o.getClass().getInterfaces();
-        for (int i = 0; i < theInterfaces.length; i++){
-            String interfaceName = theInterfaces[i].getName();
-            System.out.println(interfaceName);
-        }
     }
 }
