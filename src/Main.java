bb76429c62030ae955736903dc1e54d8852d29b8
Meral ATEŞ <202103001075@dogus.edu.tr>
2025-01-02 12:25:13 +0300

    java method overriding super keyword

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,20 @@
-import java.util.*;
-//https://www.hackerrank.com/challenges/java-iterator/problem?isFullScreen=true
-public class Main {
-    static Iterator func(ArrayList myList){
-        Iterator it = myList.iterator(); //Iterator nesnesi sayesinde listedeki elemanlara sırasıyla erişebiliriz
-        while(it.hasNext()){ //metod listedeki bir sonraki elemana geçip geçemeyeceğimizi kontrol eder
-            Object element = it.next();
-            if(element instanceof String && element.equals("###")){
-                break;
-            }
-        }
-        return it;
+class BiCylce{
+    String define_me(){
+        return "a vehicle with pedals.";
     }
-    @SuppressWarnings({"unhecked"})
+}
+class MotorCylce extends BiCylce{
+    String define_me(){
+        return "a cycle with an engine.";
+    }
+    MotorCylce(){
+        System.out.println("Hello I am motorcycle,I am"+define_me());
+        String temp=super.define_me();
+            System.out.println("My ancestor is a cycle who is "+temp);
+    }
+}
+class Main {
     public static void main(String[] args) {
-        ArrayList myList = new ArrayList();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
-        for(int i = 0; i < n; i++){
-            myList.add(sc.nextInt());
-        }
-        myList.add("###"); //Listeye "###" ekler listeyi iki bölüme ayırmak için
-        for (int i = 0; i < m; i++){
-            myList.add(sc.next());
-        }
-        Iterator it = func(myList);
-        while (it.hasNext()) {
-            Object element = it.next();
-            System.out.println((String) element);
-        }
+        MotorCylce M = new MotorCylce();
     }
 }
